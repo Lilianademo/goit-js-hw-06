e1c63afdf9245993af7b92930c67ad61f86879ea
Lilianademo <123247173+Lilianademo@users.noreply.github.com>
2023-11-09 07:02:57 +0200

    homework-js-6

---
+++ js/task-02.js
@@ -0,0 +1,17 @@
+const ingredients = [
+  'Potatoes',
+  'Mushrooms',
+  'Garlic',
+  'Tomatos',
+  'Herbs',
+  'Condiments',
+];
+let resultList = [];
+for(let ingredient of ingredients){
+  let liItem = document.createElement("li");
+  liItem.textContent = ingredient;
+  liItem.classList.add("item");
+  resultList.push(liItem);
+}
+let list = document.querySelector("#ingredients");
+resultList.forEach((item) => list.append(item));
