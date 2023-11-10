9c6452ff6d7c48cc4af5ea7e8f4ea906c874e524
Lilianademo <123247173+Lilianademo@users.noreply.github.com>
2023-11-10 06:05:31 +0200

    homework-js-6

--- js/task-06.js
+++ js/task-06.js
@@ -1,13 +1,12 @@
 let input = document.querySelector("#validation-input");
-
-input.addEventListener("blur", () =>{
-    let validLength = Number(input.dataset.validLength);
-    let insertedLength = input.value.length;
-    if(validLength = insertedLength){
-        input.classList.remove("invalid");
-        input.classList.add("valid")
-    }else{
-        input.classList.add("invalid")
-        input.classList.add("valid")
-    }
+input.addEventListener("blur", () => {
+  let validLength = Number(input.dataset.length);
+  let insertedLength = input.value.length;
+  if (validLength === insertedLength) {
+    input.classList.remove("invalid");
+    input.classList.add("valid");
+  } else {
+    input.classList.add("invalid");
+    input.classList.remove("valid");
+  }
 });
